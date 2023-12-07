b64ce99dc25bf04126df681531364fa6f7892737
sasao <sasao@lifematics.co.jp>
2023-12-07 21:51:41 +0900

    test1

--- client/deploy.sh
+++ client/deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m "New Deployment"
-git push -f git@github.com:sasao037/wav-reserve.git master:gh-pages
+git push -f git@github.com:sasao037/wav2020.git master:gh-pages
 
 cd -
